14747eadac968a72ea9363aeb7e3e6cbd371cc5a
Mani <>
2022-10-09 01:16:27 -0400

    First commit

---
+++ WslBinaryDir.cpp
@@ -0,0 +1,12 @@
+﻿// WslBinaryDir.cpp : Defines the entry point for the application.
+//
+
+#include "WslBinaryDir.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
